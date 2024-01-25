63f03fbd3a789a4de147abc2abc28b940c5ce190
jorgechvz <jorgechvzpon@gmail.com>
2024-01-25 18:11:23 -0500

    Add formatDate function and apiUrls object

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export function formateDate(date) {
+  return new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  })
+}
+
+export const apiUrls = {
+  now_playing: {
+    url: "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+    title: "Now Playing",
+  },
+  popular: {
+    url: "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    title: "What's Popular",
+  },
+  top_rated: {
+    url: "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    title: "Top Rated",
+  },
+  upcoming: {
+    url: "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+    title: "Upcoming",
+  },
+}
