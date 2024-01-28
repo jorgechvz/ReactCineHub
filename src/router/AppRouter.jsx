a77627ccff1e345dfd7f9f0e06ec956332e58021
jorgechvz <jorgechvzpon@gmail.com>
2024-01-28 02:16:44 -0500

    Refactor router component to include new pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,8 @@
 import Home from "@/pages/Home";
 import MovieDetail from "@/pages/MovieDetail";
-import MovieCategory from "@/pages/MoviesCategory";
 import { Route, Routes } from "react-router-dom";
+import AllMovies from "@/pages/AllMovies";
+import LoginPage from "@/pages/LoginPage";
 
 export const AppRouter = () => {
   return (
@@ -9,10 +10,8 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
-        <Route
-          path="/movies/list/:page?"
-          element={<MovieCategory />}
-        />
+        <Route path="/movies/list/:page?" element={<AllMovies />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
