1662da63868e8e1e3c6b0d7ada6da1b34e2bdd87
jorgechvz <jorgechvzpon@gmail.com>
2024-01-28 02:16:32 -0500

    Add LoginPage component

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Login from "@/components/login/Login";
+import Navbar from "@/components/ui/Navbar";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Navbar />
+      <Login />
+    </>
+  );
+}
