5c848456247dfa32a186e38cead4b97a824aa251
jorgechvz <jorgechvzpon@gmail.com>
2024-01-30 12:04:46 -0500

    Add filters and parameters to API request URL

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -3,8 +3,11 @@ import { formateDate } from "@/lib/utils";
 import { Link } from "react-router-dom";
 
 export default function MovieList({ page, filters }) {
+  // Url for the API request to get the list of movies
   let url = "https://api.themoviedb.org/3/discover/movie?language=en-US";
 
+  // Add the parameters to the url if they are not null
+  // Range date filter parameters are not null if the user has selected a date
   if (filters.rangeDate.from !== null) {
     url += `&primary_release_date.gte=${filters.rangeDate.from}`;
   }
@@ -13,6 +16,7 @@ export default function MovieList({ page, filters }) {
     url += `&primary_release_date.lte=${filters.rangeDate.to}`;
   }
 
+  // Vote average filter parameters are not null if the user has selected a value
   if (filters.voteAverage.from !== null) {
     url += `&vote_average.gte=${filters.voteAverage.from}`;
   }
@@ -21,6 +25,7 @@ export default function MovieList({ page, filters }) {
     url += `&vote_average.lte=${filters.voteAverage.to}`;
   }
 
+  // Runtime filter parameters are not null if the user has selected a value
   if (filters.runtime.from !== null) {
     url += `&with_runtime.gte=${filters.runtime.from}`;
   }
@@ -29,19 +34,24 @@ export default function MovieList({ page, filters }) {
     url += `&with_runtime.lte=${filters.runtime.to}`;
   }
 
+  // Genre filter parameters are not null if the user has selected a value
   if (filters.genre !== null) {
     const genreString = filters.genre.join(",");
     const encodedGenreString = encodeURIComponent(genreString);
     url += `&with_genres=${encodedGenreString}`;
   }
 
+  // Sort by filter parameter is not null if the user has selected a value
   if (filters.sortby !== null) {
     url += `&sort_by=${filters.sortby}`;
   }
 
+  // Add the page parameter to the url
   url += `&page=${page}`;
+  // Fetch the list of movies
   const { data: listMovie, loading, hasError } = useGetFetch(url);
 
+  // Render the MovieList component
   return loading ? (
     <div>Loading...</div>
   ) : (
