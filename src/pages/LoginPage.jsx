75b6e155e820bbcb5d160555b6916f1c781c5f4f
jorgechvz <jorgechvzpon@gmail.com>
2024-01-30 12:05:45 -0500

    Add render comment to LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import Login from "@/components/login/Login";
 import Navbar from "@/components/ui/Navbar";
 
 export default function LoginPage() {
+  // Render the LoginPage component
   return (
     <>
       <Navbar />
