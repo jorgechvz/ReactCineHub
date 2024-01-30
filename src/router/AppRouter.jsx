099227cc420a5aa1e28f8965222bec66d6ca8d30
jorgechvz <jorgechvzpon@gmail.com>
2024-01-30 12:05:51 -0500

    Add routes to AppRouter component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,9 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMovies from "@/pages/AllMovies";
 import LoginPage from "@/pages/LoginPage";
 
+// Define the AppRouter component
 export const AppRouter = () => {
+  // Render the AppRouter component
   return (
     <>
+      {/* Define routes for the app */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
