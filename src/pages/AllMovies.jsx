f29a22b4202c08ac649d1ef8189af10e1ec6f37e
jorgechvz <jorgechvzpon@gmail.com>
2024-01-30 12:05:38 -0500

    Refactor AllMovies component and add filter functionality

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -4,21 +4,29 @@ import Navbar from "@/components/ui/Navbar";
 import Pagination from "@/components/ui/Pagination";
 import { useState } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
-export default function AllMovies() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const { page = 1 } = useParams();
-  const navigate = useNavigate();
 
+// Define the AllMovies component
+export default function AllMovies() {
+  // Define the current page state
+  const [currentPage, setCurrentPage] = useState(1);
+  // Get the page parameter from the url
+  const { page = 1 } = useParams();
+  // Define the navigate function
+  const navigate = useNavigate();
+  // If the page parameter is not a number or is less than 1, redirect to the home page
   if (!page) {
     navigate("/");
     return <Navigate to="/" />;
   }
-
+  // Handle the page change
   const handlePageChange = (newPage) => {
+    // Update the current page state
     setCurrentPage(newPage);
+    // Navigate to the new page
     navigate(`/movies/list/${newPage}`);
   };
 
+  // Define the filters state
   const [filters, setFilters] = useState({
     genre: null,
     rangeDate: {
@@ -35,11 +43,11 @@ export default function AllMovies() {
       to: null,
     },
   });
-
+  // Handle the sort filter select
   const handleSortSelect = (sortBy) => {
     setFilters((prev) => ({ ...prev, sortby: sortBy }));
   };
-
+  // Handle the genre filter select
   const handleGenreSelect = (genreId, isChecked) => {
     setFilters((prev) => {
       const prevGenre = prev.genre || [];
@@ -50,7 +58,7 @@ export default function AllMovies() {
       }
     });
   };
-
+  // Handle the date filter select
   const handleDateSelect = (date) => {
     setFilters((prev) => ({
       ...prev,
@@ -60,7 +68,7 @@ export default function AllMovies() {
       },
     }));
   };
-
+  // Handle the vote average filter select
   const handleVoteAverageSelect = (voteAverage) => {
     setFilters((prev) => ({
       ...prev,
@@ -69,8 +77,8 @@ export default function AllMovies() {
         to: voteAverage.to,
       },
     }));
-  }
-
+  };
+  // Handle the runtime filter select
   const handleRuntimeSelect = (runtime) => {
     setFilters((prev) => ({
       ...prev,
@@ -79,7 +87,8 @@ export default function AllMovies() {
         to: runtime.to,
       },
     }));
-  }
+  };
+  // Render the AllMovies component
   return (
     <>
       <Navbar />
