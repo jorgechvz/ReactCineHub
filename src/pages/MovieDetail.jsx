96bc90118148d9b667f5e5fbaf4ac7def2c7e825
jorgechvz <jorgechvzpon@gmail.com>
2024-01-30 12:05:48 -0500

    Add movie id parameter handling and back button functionality

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -2,16 +2,20 @@ import MovieDetailRender from "@/components/movieDetail/MovieDetailRender";
 import Navbar from "@/components/ui/Navbar";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 export default function MovieDetail() {
+  // Get the movie id from the url
   const { id } = useParams();
+  // Define the navigate function
   const navigate = useNavigate();
+  // If the id parameter is not a number or is less than 1, redirect to the home page
   if (!id) {
     navigate("/");
     return <Navigate to="/" />;
   }
+  // Handle the back button click
   const onNavigateBack = () => {
     navigate(-1);
   };
-
+  // Render the MovieDetail component
   return (
     <>
       <Navbar />
